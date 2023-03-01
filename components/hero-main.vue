8f27c566918c6e866499384ff1e95183a6d88482
Lucian <la@checkmate.dk>
2023-03-01 10:33:33 +0100

    installed nuxt icon

---
+++ components/hero-main.vue
@@ -0,0 +1,18 @@
+<template>
+    <article class="theme-grid pl-10 pr-24">
+            <img class=" aspect-square object-cover" src="../assets/images/lucian.jpg" alt="Lucian">
+            <section class="flex flex-col col-start-3 justify-center">
+                <h1 class=" font-display">Frontend Developer with flair for UI/UX</h1>
+                <p>Lorem ipsum dolor sit amet aliquam eiusmod nulla fermentum interdum dictum phasellus lajoreet facilisi. Dictum ornare nunc praesent aliquam lacus lobortis dictum risus. Vulputate netus venenatis fringilla dolore duis elit aliqua nulla lobortis purus. Venenatis do iaculis lacinia aenean sagittis orci phasellus aliquet diam. Massa mollis viverra facilisis vivamus venenatis facilisi massa.</p>
+                <div class="flex gap-x-6">
+                    <button class=" text-base font-bold bg-secondary text-white py-3 px-10 my-1 rounded-md">Contact</button>
+                    <Icon class="h-full w-auto" name="uil:linkedin" />
+                    <Icon class="h-full w-auto" name="uil:github" />
+                </div>
+            </section>
+        </article>
+</template>
+
+<script>
+export default {};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,13 @@
 <template>
-    <div class="pt-10">
-        <article class="theme-grid pl-10 pr-24">
-            <img class=" aspect-square object-cover" src="../assets/images/lucian.jpg" alt="Lucian">
-            <section class="flex flex-col col-start-3 justify-center">
-                <h1 class=" font-display">Frontend Developer with flair for UI/UX</h1>
-                <p>Lorem ipsum dolor sit amet aliquam eiusmod nulla fermentum interdum dictum phasellus lajoreet facilisi. Dictum ornare nunc praesent aliquam lacus lobortis dictum risus. Vulputate netus venenatis fringilla dolore duis elit aliqua nulla lobortis purus. Venenatis do iaculis lacinia aenean sagittis orci phasellus aliquet diam. Massa mollis viverra facilisis vivamus venenatis facilisi massa.</p>
-                <div>
-                    <button class=" text-base font-bold bg-secondary text-white py-3 px-10 rounded-lg">Contact</button>
-                </div>
-            </section>
-        </article>
+    <div class="pt-8">
+        <hero-main />
+        
     </div>
 </template>
 
 <script>
-export default {};
+import heroMain from "~~/components/hero-main.vue";
+export default {
+  components: { heroMain },
+};
 </script>
